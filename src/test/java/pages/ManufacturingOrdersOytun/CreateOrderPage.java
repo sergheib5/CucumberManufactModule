874775316ca77f11729a6232bed1a630b0370210
oytun <oytunpiren@gmail.com>
2020-02-29 19:04:07 -0600

    first

--- src/test/java/pages/ManufacturingOrdersOytun/CreateOrderPage.java
+++ src/test/java/pages/ManufacturingOrdersOytun/CreateOrderPage.java
@@ -1,13 +1,12 @@
-package pages;
+package pages.ManufacturingOrdersOytun;
 
-import utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 
-public class ManufacturingOrders {
-
-    public ManufacturingOrders() {
+public class CreateOrderPage {
+    public CreateOrderPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
@@ -45,25 +44,5 @@ public class ManufacturingOrders {
     public WebElement saveButtOn;
 
 
-    @FindBy(xpath = "(//input[@type='checkbox'])[4]")
-    public WebElement pickOrder;
-
-    @FindBy(xpath = "(//button[@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[1]")
-    public WebElement printButton;
-
-    @FindBy(xpath = "(//button[@class='o_dropdown_toggler_btn btn btn-sm dropdown-toggle'])[2]")
-    public WebElement actionButton;
-
-    @FindBy(xpath = "(//ul[@class='dropdown-menu'])[4]//a[@data-index='1']")
-    public WebElement deleteButton;
-
-    @FindBy(xpath = "//button[@class='btn btn-sm btn-primary']")
-    public WebElement okayButtinForDelete;
-
-    @FindBy(xpath =  "//div[@class='o_dialog_warning modal-body']")
-public  WebElement actualResult;
-
-@FindBy (xpath = "//button[@class='btn btn-sm btn-primary']")
-public WebElement finishButton;
 
 }
